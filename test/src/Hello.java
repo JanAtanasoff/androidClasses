c0cc1d2ea4519637aec787658c126838e8175eb3
JanAtanasoff <yanko.atanasov@gmail.com>
2012-03-07 11:49:57 +0100

    Update test/src/Hello.java

--- test/src/Hello.java
+++ test/src/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
 	 * @param args, here none
 	 */
 	public static void main(String[] args){
-		String s = "Hello World, this is me - Lukas : ))";
+		String s = "NIce Work";
 		System.out.println(s);
 	}
 	
