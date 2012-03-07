e998a4caa9045d9064a69e274e6b37dadda11f3f
lukassero <elgradzki@tlen.pl>
2012-03-07 11:17:31 +0100

    What is up Yo?!

---
+++ test/src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+	/*
+	 * @param args, here none
+	 */
+	public static void main(String[] args){
+		String s = "Hello World, this is me - Lukas : ).";
+		System.out.println(s);
+	}
+	
+}
